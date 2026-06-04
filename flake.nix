a5b25bde4208dd579ceb2d37dd6295c2daa8ec66
Kevin Stone <kevinastone@gmail.com>
2026-06-03 21:20:46 -0700

    ci: build and push multi-architecture container images using qemu and regctl

--- flake.nix
+++ flake.nix
@@ -57,6 +57,64 @@
             inherit cargoArtifacts;
           }
         );
+
+        push-multiarch = pkgs.writeShellApplication {
+          name = "push-multiarch";
+          runtimeInputs = with pkgs; [
+            regctl
+            gzip
+            coreutils
+          ];
+          text = ''
+            if [ "$#" -lt 2 ]; then
+              echo "Usage: push-multiarch <amd64-image-tar> <arm64-image-tar>"
+              exit 1
+            fi
+
+            AMD64_IMAGE="$1"
+            ARM64_IMAGE="$2"
+
+            if [ -z "''${TAGS:-}" ]; then
+              echo "Error: TAGS environment variable is not set"
+              exit 1
+            fi
+            if [ -z "''${REPO:-}" ]; then
+              echo "Error: REPO environment variable is not set"
+              exit 1
+            fi
+
+            # Import images into local OCI layout directories directly from Nix build outputs
+            regctl image import ocidir://./local-oci-amd64 "$AMD64_IMAGE"
+            regctl image import ocidir://./local-oci-arm64 "$ARM64_IMAGE"
+
+            # Get the digests of the imported OCI layouts
+            AMD64_DIGEST=$(regctl image digest ocidir://./local-oci-amd64)
+            ARM64_DIGEST=$(regctl image digest ocidir://./local-oci-arm64)
+
+            # Push single-architecture layers and manifests by digest
+            echo "Pushing AMD64 digest: $AMD64_DIGEST to $REPO..."
+            regctl image copy ocidir://./local-oci-amd64 "$REPO@$AMD64_DIGEST"
+
+            echo "Pushing ARM64 digest: $ARM64_DIGEST to $REPO..."
+            regctl image copy ocidir://./local-oci-arm64 "$REPO@$ARM64_DIGEST"
+
+            # Create and push the multi-architecture manifest index for each tag
+            # Since TAGS is multiline, we read it line by line
+            echo "$TAGS" | while read -r tag || [ -n "$tag" ]; do
+              if [ -n "$tag" ]; then
+                echo "Creating and pushing multi-arch index for $tag..."
+                regctl index create "$tag" \
+                  --ref "$REPO@$AMD64_DIGEST" \
+                  --platform linux/amd64 \
+                  --ref "$REPO@$ARM64_DIGEST" \
+                  --platform linux/arm64
+              fi
+            done
+
+            # Cleanup local OCI layouts
+            rm -rf ./local-oci-amd64 ./local-oci-arm64
+          '';
+        };
       in
       rec {
         packages = rec {
@@ -107,7 +165,7 @@
               };
             };
 
-          inherit (pkgs) skopeo;
+          inherit push-multiarch;
         };
 
         checks = {
@@ -121,7 +179,6 @@
           craneLib.devShell {
             checks = self.checks.${system};
             packages = [
-              skopeo
               cargo-outdated
               cargo-release
               git-cliff
